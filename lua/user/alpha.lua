6b08a4e26164de2bdc90086509fa91f6b62f4f99
Sumit Dhiman <76207887+lucifer1708@users.noreply.github.com>
2022-03-17 12:51:09 +0530

    Update alpha.lua

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -30,7 +30,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return " ～～ Ｉ ＵＳＥ ＡＲＣＨ ＢＴＷ ～～"
+	return "~~ I use arch btw ~~"
 end
 
 dashboard.section.footer.val = footer()
