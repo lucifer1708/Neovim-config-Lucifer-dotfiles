c5dd971926399d793e86b3d55dedf624ca533eeb
lucifer1708 <20bce091@nith.ac.in>
2022-03-19 13:32:15 +0530

    emmet improved

--- lua/user/lsp/nvim-lspconfig.lua
+++ lua/user/lsp/nvim-lspconfig.lua
@@ -1,13 +1,21 @@
 local lspconfig = require'lspconfig'
-local configs = require'lspconfig/configs'   
+local configs = require'lspconfig.configs'
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-lspconfig.emmet_ls.setup({
-    -- on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "html", "css", "typescriptreact", "javascriptreact", },
-})
+if not configs.ls_emmet then
+  configs.ls_emmet = {
+    default_config = {
+      cmd = { 'ls_emmet', '--stdio' };
+      filetypes = { 'html', 'css', 'scss', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'haml',
+        'xml', 'xsl', 'pug', 'slim', 'sass', 'stylus', 'less', 'sss'};
+      root_dir = function(fname)
+        return vim.loop.cwd()
+      end;
+      settings = {};
+    };
+  }
+end
 
-
+lspconfig.ls_emmet.setup{ capabilities = capabilities }
